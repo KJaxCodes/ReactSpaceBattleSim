ec3778b844cff15cc86f32d7b8e7e413a3e7a70c
Kelly J <[kelly4410@gmail.com]>
2025-03-21 13:56:03 -0600

    update fire button and damage

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Battle from './Battle'
 
-//States - GameStatus is a state, PlayerHealth is a state, EnemyHealth is a state ???
-
-const InitialHealth = 100;
-const GameStatus = ""; //active, win, loss, draw - connected to firebutton or restartbutton, game message
-
-const PlayerHealth = InitialHealth;
-const EnemyHealth = InitialHealth;
-
-function FireButton
-
-    //onClick
-    //RandomDamage, Max and Min limits
-    //Re-render PlayerHealth and EnemyHealth based on damage
-
-
- //Conditional: If PlayerHealth or EnemyHealth is less than or equal to 0, gameover
- // if both are 0, Stalemate message and RestartButton displays.
- // If PlayerHealth is less than or equal to 0, gameover, and if EnemyHealth is greater than 0, You lost message and RestartButton displays.
- // If EnemyHealth is less than or equal to 0, gameover, and if PlayerHealth is greater than 0, You won message and RestartButton displays.
-
-function RestartButton
-
-function GameMessage = ""; //"Congratulations you won!" "Stalemate, both battleships down" "You lost, better luck next time!"
 
 
 function App() {
-  
+
 
   return (
     <>
@@ -35,12 +13,10 @@ function App() {
         <h1>Space Battle Simulator</h1>
       </div>
       <div>
-        PlayerHealth
-        FireButton / RestartButton
-        EnemyHealth
+        <Battle />
       </div>
       <div>
-        GameMessage
+        {/* {GameMessage} */}
       </div>
 
 
---
+++ src/Battle.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+function Battle() {
+    //set state to update playerPoints and enemyPoints, both start at 100
+    const [points, setPoints] = useState({ playerPoints: 100, enemyPoints: 100 });
+    //create random num generator to generate random damage
+    function randomDamage() {
+        return Math.floor(Math.random() * 100);
+    }
+
+
+    //fire button onclick function that will copy the points objects and update it with the randomDamage
+    function applyDamage() {
+        const newPoints = { ...points, playerPoints: points.playerPoints - randomDamage(), enemyPoints: points.enemyPoints - randomDamage() }
+        setPoints(newPoints);
+    }
+
+    //do i need a separate button that is a reset button?
+    function reset() {
+        setPoints({ playerPoints: 100, enemyPoints: 100 })
+    }
+
+    return (
+        <div>
+            <span>Player Health: {points.playerPoints}</span>
+            <button className="fire" onClick={applyDamage}>Fire</button>
+            <span>Enemy Health: {points.enemyPoints}</span>
+        </div>
+    )
+}
+
+export default Battle;
+
+// const diceArr = [];
+// for (let i = 0; i < numDice; i++) {
+//     const num = Math.floor(Math.random() * maxVal) + 1;
+//     //gives a random number from 1 - 20
+//     diceArr.push(num)
+// }
+
+// function randomEmoji() {
+//     const choices = ["🤖", "😸", "😘", "👽", "🧙🏼‍♀️", "🦄"];
+//     return choices[Math.floor(Math.random() * choices.length)];
+// }
+
+
+
+//States - GameStatus is a state, PlayerHealth is a state, EnemyHealth is a state ???
+
+// const InitialHealth = 100;
+// const GameStatus = ""; //active, win, loss, draw - connected to firebutton or restartbutton, game message
+
+// const PlayerHealth = InitialHealth;
+// const EnemyHealth = InitialHealth;
+
+// function FireButton
+
+//onClick
+//RandomDamage, Max and Min limits??
+//Re-render PlayerHealth and EnemyHealth based on damage
+
+
+//Conditional: If PlayerHealth or EnemyHealth is less than or equal to 0, gameover
+// if both are 0, Stalemate message and RestartButton displays.
+// If PlayerHealth is less than or equal to 0, gameover, and if EnemyHealth is greater than 0, You lost message and RestartButton displays.
+// If EnemyHealth is less than or equal to 0, gameover, and if PlayerHealth is greater than 0, You won message and RestartButton displays.
+
+// function RestartButton
+
+// function GameMessage = ""; //"Congratulations you won!" "Stalemate, both battleships down" "You lost, better luck next time!"
